6e7a14b9d77660f37fc161185049310a8f052b76
mplatypus <contact@mplaty.com>
2026-03-01 22:24:56 +1100

    Add changelog and bump version

--- ongaku/internal/about.py
+++ ongaku/internal/about.py
@@ -9,7 +9,7 @@ __author_email__: typing.Final[str] = "contact@mplaty.com"
 __maintainer__: typing.Final[str] = "MPlaty"
 __license__: typing.Final[str] = "MIT"
 __url__: typing.Final[str] = "https://github.com/hikari-ongaku/hikari-ongaku"
-__version__: typing.Final[str] = "1.0.3"
+__version__: typing.Final[str] = "1.0.4"
 
 # MIT License
 
